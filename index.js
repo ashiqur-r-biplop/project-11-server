1a89a4cce6182afc4b94c5f01fa4b04b7166a769
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-10-19 00:22:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// middleware
+app.use(cors());
+app.use(express.json());
+const username = process.env.USER_NAME;
+const password = process.env.PASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.klmvqmu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  // console.log(authorization);
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+  // console.log(token);
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access2" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    const userCollection = client.db("job-portal").collection("users");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      // console.log(email);
+      const query = { email: email };
+      // console.log(query);
+      const user = await userCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+    // student
+    const verifyHiringManager = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      // console.log(query);
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      if (user?.role !== "hiringManager") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+    // instructor
+    const verifyJobSeeker = async (req, res, next) => {
+      const email = req.decoded;
+      // console.log(email);
+      const query = { email: email.email };
+      // console.log(query, "email");
+      const user = await userCollection.findOne(query);
+      // console.log(user);
+      if (user?.role !== "jobSeeker") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("testing server");
+});
+
+app.listen(port, () => {
+  console.log(`CreativaDesignHub is sitting on port ${port}`);
+});
